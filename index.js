5f91dcdc327e62576c4f78e90a35a85ffdcfecc4
Jacob Hauberg Hansen <jacob.hauberg@gmail.com>
2014-05-20 11:06:44 +0200

    Hide content initially while loading.

--- index.js
+++ index.js
@@ -5,43 +5,54 @@ function fidget(element, range) {
     rotate(element, rotation);
 }
 
-/*
-    Zooms in on a card. All other cards go back to normal.
-*/
-function toggleZoomOnCard(card) {
-    $(card).attr('style', '');
+function toggleFanOut(card) {
+    var deck = $('#deck');
 
-    if ($(card).hasClass('normal')) {
-        $(card).removeClass('normal');
-        $(card).addClass('zoom');
-    } else if ($(card).hasClass('zoom')) {
-        $(card).removeClass('zoom');
-        $(card).addClass('normal');
+    card.attr('style', '');
+    deck.attr('style', '');
+
+    if (card.hasClass('normal')) {
+        card.removeClass('normal');
+        card.addClass('zoom');
+    } else if (card.hasClass('zoom')) {
+        card.removeClass('zoom');
+        card.addClass('normal');
     } else {
-        $(card).addClass('zoom');
+        card.addClass('zoom');
+    }
+
+    if (deck.hasClass('normal')) {
+        deck.removeClass('normal');
+        deck.addClass('zoom-2');
+    } else if (deck.hasClass('zoom-2')) {
+        deck.removeClass('zoom-2');
+        deck.addClass('normal');
+    } else {
+        deck.addClass('zoom-2');
     }
 }
 
 $("#trickbook").on('touchend mouseenter', function() {
-    toggleZoomOnCard($(this));
-
-    deck = $('#deck');
-
-    deck.attr('style', '');
-    deck.removeClass('normal');
-    deck.addClass('zoom-2');
+    toggleFanOut($(this));
 });
 
 $("#trickbook").on('mouseleave', function() {
-    toggleZoomOnCard($(this));
-
-    deck = $('#deck');
-
-    deck.attr('style', '');
-    deck.removeClass('zoom-2');
-    deck.addClass('normal');
+    toggleFanOut($(this));
 });
 
 $('.trick').each(function(index) {
     fidget($(this), 3);
+});
+
+$(window).load(function() {
+    // run the fan out/in animations initially (while hidden) to avoid artifacts
+    toggleFanOut($('#trickbook'));
+
+    setTimeout(function() {
+        toggleFanOut($('#trickbook'));
+
+        setTimeout(function() {
+            $("body").fadeIn(250);
+        }, 100);
+    }, 100);
 });
